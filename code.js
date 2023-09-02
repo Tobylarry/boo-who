8253aa55d8f0dd887e667eb3ca1e5a93d5fbbd8d
Tobi <47112773+Tobylarry@users.noreply.github.com>
2023-09-01 23:40:12 -0700

    Create code.js
    Check if a value is classified as a boolean primitive. Return true or false.
    
    Boolean primitives are true and false.
    
    Solution:

---
+++ code.js
@@ -0,0 +1,8 @@
+// make you of the typeof method to check datatype
+//create a function
+//here we make use of (typeof method to check data types and returns true if boolean else false....
+function booWho(bool) {
+  return typeof bool === "boolean" ? true : false; // checks data type to see if it is boolean and returns true or false....
+}
+
+booWho(null);
